d9441dd353f7c12ab1baea31a6a6d181071d3135
Alrun <fkhey2016@yandex.ru>
2017-04-16 15:19:49 +0300

    repair

--- dist/js/main.js
+++ dist/js/main.js
@@ -16,12 +16,37 @@ $(document).ready(function(){
 		fade: true,
 		asNavFor: '.slider-main'
 	});
-	$('.slider-work').slick({
-		slidesToShow: 3,
-		slidesToScroll: 3,
-		arrows: true,
-		dots: true
-	});
+$('.slider-work').slick({
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    arrows: true,
+    dots: true,
+    infinite: false,
+    speed: 300,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        }
+      },
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
 
 });
 ;( function( window, document ) {
