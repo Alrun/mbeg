b53d5d3594fbc7b862fd2b58c9aa004293b06824
Alrun <fkhey2016@yandex.ru>
2017-04-16 15:54:38 +0300

    repair rep

--- app/js/common.js
+++ app/js/common.js
@@ -26,32 +26,32 @@ $(document).ready(function(){
 		slidesToShow: 3,
 		slidesToScroll: 3,
 		arrows: true,
-    dots: true,
-    infinite: false,
-    speed: 300,
-    responsive: [
-      {
-        breakpoint: 992,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-        }
-      },
-      {
-        breakpoint: 991,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
-        }
-      },
-      {
-        breakpoint: 767,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
-  });
+		dots: true,
+		infinite: false,
+		speed: 300,
+		responsive: [
+		{
+			breakpoint: 992,
+			settings: {
+				slidesToShow: 3,
+				slidesToScroll: 3,
+			}
+		},
+		{
+			breakpoint: 991,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 2
+			}
+		},
+		{
+			breakpoint: 767,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1
+			}
+		}
+		]
+	});
 
 });
